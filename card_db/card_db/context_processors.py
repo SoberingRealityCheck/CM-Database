80d1c186d1229fe5e76f86eb5a326efb45c2f070
Clay G <buzzscoutbark@gmail.com>
2024-08-03 10:33:52 -0500

    local changes

--- card_db/card_db/context_processors.py
+++ card_db/card_db/context_processors.py
@@ -2,4 +2,4 @@ from django.conf import settings
 import os
 
 def site(request):
-    return {'SITE_URL': os.path.join(settings.STATIC_URL,'../hgcal/cm')}
+    return {'SITE_URL': os.path.join(settings.STATIC_URL,'../')}
